038097b89b564202c681c198105c00cac7e3c9ad
MateuuzB <mateusbenedetto7@gmail.com>
2020-06-07 23:10:52 -0300

    NextLevelWeek
    Next Level Week - Criação do meu primeiro site com HTTP / CSS / JAVASCRIPT

---
+++ public/Scripts/intro.js
@@ -0,0 +1,64 @@
+//coments
+//document.write ("hello")
+
+// variaveis, tipos de dados
+//let myvar //undefined
+//myvar = "he"
+//const myconst = "Ge"
+//document.write(myconst + myvar)
+
+//string
+//"Isso é uma string"
+//'Isso também é uma string'
+
+//number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+//boolean - true or false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+//objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//    document.write("executar uma grande logica de correr")
+//   }
+// }
+// pessoa.correr()
+
+//Array - Vetores
+// const colecaodebolinhas = ["blue", "green", 1, {name:"my name"}]
+// document.write(colecaodebolinhas[3].name)
+
+//funções - funcionalidades - atalhos - reuso de codigos 
+// function sayMyName(name) {
+//   document.write(name)
+// }
+// //executar
+// sayMyName("pica")
+// sayMyName("oloca")
+// sayMyName("teteu")
+// sayMyName("pirulito")
+
+//condicionais
+// const notafinal = 
+
+// if( notafinal <5 ) {
+//   //caminho 1
+//   document.write("reprovado")
+// } else {
+//   document.write("aprovado")
+//   //caminho 2
+// }
+
+//loop - repetições
+
+// for(i = 0; i < 99; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
